e4be01d14d99ea01178610eb23e979550d1dd4c7
Kris Jones <kristop.jones@gmail.com>
2022-07-13 19:43:52 -0600

    first commit

---
+++ TowerApp.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-1s3xbyye.us.auth0.com'
+export const clientId = 'TVhy9oeDU6DldnY9LTS587vIW9Z1el5D'
+export const audience = 'https://towerapp'
