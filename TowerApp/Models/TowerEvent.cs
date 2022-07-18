c1b5ea8c6a7dac210613cecbd5d802e8936faf5e
Kris Jones <kristop.jones@gmail.com>
2022-07-18 17:54:35 -0600

    update model

--- TowerApp/Models/TowerEvent.cs
+++ TowerApp/Models/TowerEvent.cs
@@ -6,7 +6,6 @@ namespace TowerApp.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public string EventImage { get; set; }
-
         public int Tickets { get; set; }
         public string EventType { get; set; }
         public string Location { get; set; }
